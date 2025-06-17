6bf74955fe5f6ce2178f2401aaae489c590309f6
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-06-17 15:54:13 +0530

    Update ClientTestimonialSlider.tsx

--- components/client/ClientTestimonialSlider.tsx
+++ components/client/ClientTestimonialSlider.tsx
@@ -166,7 +166,7 @@ const ClientTestimonialSlider = ({
       </div>
 
      {/* Video Section */}
-<div className="video-section w-full px-6 lg:px-12 py-2">
+<div className="video-section w-full px-6 lg:px-7 py-2">
   <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
     {videoData.map((video, index) => (
       <div
