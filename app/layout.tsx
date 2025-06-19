27b87890b13a1ad0b07fd988e82deabaac6c5de6
ThynkRahul <thynk.rahul@gmail.com>
2025-06-19 19:47:20 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ import LandingNavBar from "../components/LandingNavBar"; // Specific Navbar for 
 import Footer from "../components/Footer";
 import CanonicalURL from "../components/CanonicalURL";
 import logo from "../public/images/logo.png";
-import GoogleTranslate from '@/components/GoogleTranslate';
+import GoogleTranslate from "../components/GoogleTranslate";
 
 const homePageJsonLd = {
   "@context": "https://schema.org",
